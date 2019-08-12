5a5a20f022e289c09d74b4d0eab46bed02cddbf1
Randy Taylor <randyisbusy@gmail.com>
2019-08-12 17:28:54 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word = word
+  end
+  def match(array)
+    gel = word.split("").to_a.sort{ |x,y| x <=> y }
+    answers = []
+    array.each do |maybe|
+      nice = maybe.split('').to_a.sort{ |x,y| x <=> y }
+      nice == gel ? answers << maybe : nil
+    end
+    answers
+  end
+
+end
