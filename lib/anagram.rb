06ad588a8a74beca00e2a89ef6a8e5f7042fe19c
Randy Taylor <randyisbusy@gmail.com>
2019-08-12 17:01:38 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram 
+  attr_accessor :word 
+  def initialize(word)
+    @word = word 
+  end 
+  def match(array)
+    word.each_char do |x| 
+      x == array 
+    binding.pry 
+  end
+  end
+end
